41d399a4be5cd00288b1e32faa5a0e22afd471ee
Sreeram S S <89720320+sreeramss@users.noreply.github.com>
2024-04-10 22:21:08 +0530

    Update createDatabase.js

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -3,7 +3,7 @@ const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://sreeramnov3:Sreeram@2001@cluster0.cw5rsvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
